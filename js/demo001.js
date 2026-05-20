4d5e18c54a6825170d0e67eaf0d5165e35032052
mis-xv <kala980108@gmail.com>
2026-05-19 19:19:53 -0600

    Add files via upload

---
+++ js/demo001.js
@@ -0,0 +1,578 @@
+window.sr = ScrollReveal();
+
+	sr.reveal('.boletos', {
+		duration: 3500, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		distance: '200px' //distancia
+
+
+	}); 
+
+	sr.reveal('.nombres', {
+		duration: 2500, //tiempo
+		delay:1500,
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+
+
+	}); 
+	sr.reveal('.nombres2', {
+		duration: 3500, //tiempo
+		delay:0,
+		origin: 'bottom', //origen
+		distance: '2px' //distancia
+
+
+	}); 
+	sr.reveal('#nombres2', {
+		duration: 3500, //tiempo
+		delay:1000,
+		origin: 'bottom', //origen
+		distance: '2px' //distancia
+
+
+	}); 
+	
+	sr.reveal('.respetuosamente', {
+		duration: 2500, //tiempo
+		delay:1000,
+		origin: 'bottom', //origen
+		distance: '70px' //distancia
+
+
+	}); 
+		sr.reveal('.btnWhatsNovios', {
+		duration: 3000, //tiempo
+		delay:1000,
+		origin: 'left', //origen
+		distance: '200px' //distancia
+
+
+	}); 
+		sr.reveal('#whatsllamada', {
+		duration: 3000, //tiempo
+		delay:1000,
+		origin: 'bottom', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+sr.reveal('.vl', {
+		duration: 2000, //tiempo
+		delay:1300,
+		origin: 'top', //origen
+		distance: '100px', //distancia
+		
+
+	});
+sr.reveal('.vl2', {
+		duration: 2000, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		distance: '100px', //distancia
+		
+
+	});
+sr.reveal('.vl3', {
+		duration: 2000, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		distance: '100px', //distancia
+		
+
+	});
+	sr.reveal('.nuestraBoda', {
+		duration: 2000, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+		
+
+	});
+	sr.reveal('.textoNosCasamos', {
+		duration: 3000, //tiempo
+		delay:1500,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+	sr.reveal('.tespero', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+	sr.reveal('.tespero2', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+	sr.reveal('.tespero3', {
+		duration: 2500, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '50px' //distancia
+
+
+	}); 
+		sr.reveal('.dia', {
+		duration: 2000, //tiempo
+		delay:800,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+		sr.reveal('.mes', {
+		duration: 1500, //tiempo
+		delay:2000,
+		origin: 'left', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+		sr.reveal('.año', {
+		duration: 2500, //tiempo
+		delay:800,
+		origin: 'bottom', //origen
+		distance: '100px' //distancia
+
+
+	}); 
+			sr.reveal('#cuenta', {
+		duration: 1500, //tiempo
+		//delay:800,
+		origin: 'bottom', //origen
+		distance: '100px', //distancia
+		
+
+	});
+	sr.reveal('.ceremoniaReligiosa', {
+		duration: 1500, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.30,
+		distance: '-0' //distancia
+		
+
+	});
+	sr.reveal('.mapas', {
+		duration: 2000, //tiempo
+		//delay:100,
+		origin: 'top', //origen
+		distance: '200px', //distancia
+		
+
+	});
+	sr.reveal('.vestimenta', {
+		duration: 3000, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		scale: 0.30,
+		distance: '-0' //distancia
+		
+
+	});
+	sr.reveal('.imagen1', {
+		duration: 1000, //tiempo
+		//delay:800,
+		origin: 'top', //origen
+		distance: '300px', //distancia
+		
+
+	});
+	sr.reveal('.collage', {
+		duration: 1000, //tiempo
+		///delay:1000,
+		origin: 'top', //origen
+		scale: 0.50,
+		distance: '-0' //distancia
+		
+
+	});
+		sr.reveal('.imagen3', {
+		duration: 1000, //tiempo
+		//delay:500,
+		origin: 'bottom', //origen
+		distance: '200px', //distancia
+		
+
+	});
+
+			sr.reveal('.apellidos', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+
+			sr.reveal('.form-register', {
+		duration: 1000, //tiempo
+		//delay:800,
+		origin: 'top', //origen
+		distance: '300px', //distancia
+		
+
+	});
+			sr.reveal('.btnWhats', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.nosCasamos', {
+		duration:2500, //tiempo
+		delay:600,
+		origin: 'bottom', //origen
+		distance: '400px', //distancia
+		
+
+	});
+
+sr.reveal('.dondeyCuando', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+sr.reveal('.dondeyCuando2', {
+		duration: 4000, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '-10px', //distancia
+		
+
+	});
+sr.reveal('.Tituloasistencia', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+sr.reveal('.Tituloasistenciaespero', {
+		duration: 1500, //tiempo
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+sr.reveal('.porqueformas', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.lugar', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'top', //origen
+		distance: '-100px', //distancia
+		
+	}); 
+sr.reveal('.hora', {
+		duration: 4500, //tiempo
+		delay:900,
+		origin: 'bottom', //origen
+		distance: '20px' //distancia
+
+
+	}); 
+sr.reveal('.dondeycuandotitulo', {
+		duration: 4000, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '-10px', //distancia
+
+
+	}); 
+sr.reveal('.textodondeycuando', {
+		duration: 1500, //tiempo
+		delay:900,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.btn', {
+		duration: 3000, //tiempo
+		delay:600,
+		origin: 'bottom', //origen
+		distance: '200px' //distancia
+
+
+	}); 
+sr.reveal('.nompadres', {
+		duration: 2500, //tiempo
+		delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.tiendas', {
+		duration: 1500, //tiempo
+		//delay:700,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	}); 
+sr.reveal('.card', {
+		duration: 3500, //tiempo
+		delay:900,
+		origin: 'left', //origen
+		scale: 0.10,
+		distance: '300px' //distancia
+
+
+	}); 
+
+
+sr.reveal('.textoSeccion3', {
+		duration: 1500, //tiempo
+		origin: 'left', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	});
+	sr.reveal('.piepagina', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		reset:1000
+
+	}); 
+
+
+	sr.reveal('.table-responsive', {
+		duration: 1500, //tiempo
+		origin: 'bottom', //origen
+		distance: '-100px', //distancia
+		
+
+	});
+
+		sr.reveal('.cuentaRegresiva2', {
+		duration: 2000, //tiempo
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+
+
+	}); 
+			sr.reveal('.itinerario', {
+		duration: 2000, //tiempo
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+
+
+	});
+			sr.reveal('.banco', {
+		duration: 2000, //tiempo
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+
+
+	}); 
+		
+	sr.reveal('#hotel2', {
+			duration: 2000, //tiempo
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+
+
+
+	}); 
+	sr.reveal('#hotel3', {
+			duration: 2000, //tiempo
+		origin: 'top', //origen
+		distance: '400px' //distancia
+
+
+
+	}); 
+sr.reveal('.evitarDress', {
+	
+	}); 
+sr.reveal('.evitarDress2', {
+		duration: 3000, //tiempo
+		delay:700,
+		origin: 'bottom', //origen
+		
+		distance: '300px' //distancia
+
+
+	}); 
+	sr.reveal('.flores', {
+		duration: 3000, //tiempo
+		delay:500,
+		origin: 'top', //origen
+		distance: '300px' //distancia
+		
+
+	});
+		sr.reveal('#leon', {
+		duration: 3000, //tiempo
+		delay:900,
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+		
+
+	});
+			sr.reveal('#leon2', {
+		duration: 5500, //tiempo
+		delay:400,
+		origin: 'top', //origen
+		distance: '300px' //distancia
+		
+
+	});
+			sr.reveal('#musicaboton', {
+		rotate: {
+        x: 100,
+        z: 100
+    },
+		duration: 2000, //tiempo
+		//delay:1500,
+		origin: 'top', //origen
+		scale: 0.10,
+		distance: '-0' //distancia
+
+
+	});
+		sr.reveal('.pampas', {
+		duration: 3000, //tiempo
+		delay:500,
+		origin: 'bottom', //origen
+		distance: '300px' //distancia
+		
+
+	});
+
+			sr.reveal('.codigo', {
+		duration: 3000, //tiempo
+		delay:900,
+		origin: 'left', //origen
+		distance: '100px' //distancia
+		
+
+	});
+			sr.reveal('.codigo2', {
+		duration: 3000, //tiempo
+		delay:1000,
+		origin: 'top', //origen
+		distance: '200px' //distancia
+		
+
+	});
+			sr.reveal('.codigo3', {
+		duration: 3000, //tiempo
+		delay:1000,
+		origin: 'right', //origen
+		distance: '50px' //distancia
+		
+
+	});
+
+			sr.reveal('.formal', {
+		duration: 3500, //tiempo
+		delay:1000,
+		origin: 'bottom', //origen
+		distance: '200px' //distancia
+		
+
+	});
+
+	
+
+const counts = document.querySelectorAll('.dia')
+const speed = 10
+
+setTimeout(() => {
+  console.log("1 Segundo esperado")
+
+  counts.forEach((counter) => {
+    function upDate(){
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed        
+        if(count < target){
+
+        	
+            counter.innerText = Math.floor(inc + count) 
+            setTimeout(upDate, 100)
+        }else{
+            counter.innerText = target
+        }
+    }
+    upDate()
+})
+
+
+}, 6000);
+
+
+       
+       // Open the Modal
+function openModal() {
+  document.getElementById("myModal").style.display = "block";
+}
+
+// Close the Modal
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
+}
